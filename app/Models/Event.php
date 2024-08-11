ec5a8e31fad325b66c11d6ba6d0854582ab7debe
JhoniPS <jhonisilva500@gmail.com>
2024-08-11 19:46:58 -0300

    first commit

---
+++ app/Models/Event.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'items' => 'array',
+        'date' => 'datetime'
+    ];
+
+    protected $guarded = [];
+
+    public function user (): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+@section('title', "Dashboard")
+
+@section('content')
+    <div class="col-md-10 offset-md-1 dashboard-title-container">
+        <h1>Meus Eventos</h1>
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
+        @if(count($events) > 0)
+
+        @else
+            <p>Você não possui eventos, <a href="/event/create">Criar eventos</a></p>
+        @endif
+    </div>
+@endsection
